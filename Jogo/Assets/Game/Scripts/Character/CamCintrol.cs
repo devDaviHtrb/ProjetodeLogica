ca1581f929a9f2627a1e55d0342c627c4b5c87e4
o-deive <118860684+o-deive@users.noreply.github.com>
2024-10-17 16:19:32 -0300

    Versao1Fase1

--- Jogo/Assets/Game/Scripts/Character/CamCintrol.cs
+++ Jogo/Assets/Game/Scripts/Character/CamCintrol.cs
@@ -9,7 +9,7 @@ public class CamCintrol : MonoBehaviour
     private Transform target;
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x,gameObject.transform.position.y, transform.position.z);
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
     }
 }
 
