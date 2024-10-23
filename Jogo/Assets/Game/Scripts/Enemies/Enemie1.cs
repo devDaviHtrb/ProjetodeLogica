57903cf8267188159394a7dd04e683ecda4d5607
devDaviHtrb <davideleteribeiro1@gmail.com>
2024-10-23 11:03:20 -0300

    limpando o código

--- Jogo/Assets/Game/Scripts/Enemies/Enemie1.cs
+++ Jogo/Assets/Game/Scripts/Enemies/Enemie1.cs
@@ -44,15 +44,9 @@ public class Enemie1 : MonoBehaviour
         }
         if(vida == 0){
             Destroy(gameObject);
-        }
-        
-            
+        }  
 
     }
-    /// <summary>
-    /// OnTriggerEnter is called when the Collider other enters the trigger.
-    /// </summary>
-    /// <param name="other">The other Collider involved in this collision.</param>
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Check"){
