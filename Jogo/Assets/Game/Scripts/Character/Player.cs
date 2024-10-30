35ad172704d65e9ce22aa8908fc10e9b92711996
devDaviHtrb <davideleteribeiro1@gmail.com>
2024-10-30 18:25:05 -0300

    Arrumando o bug de velocidade do player

--- Jogo/Assets/Game/Scripts/Character/Player.cs
+++ Jogo/Assets/Game/Scripts/Character/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {  
 
-    public float speed;
+    private float speed = 0.1f;
     public float jumpF = 14f;
     private bool podepular;
     private bool doublejump;
