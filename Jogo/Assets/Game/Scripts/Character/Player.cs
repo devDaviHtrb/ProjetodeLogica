eb527b1bd218eff6c7d2e30130f463a2a29379af
devDaviHtrb <davideleteribeiro1@gmail.com>
2024-11-02 18:05:14 -0300

    1.5

--- Jogo/Assets/Game/Scripts/Character/Player.cs
+++ Jogo/Assets/Game/Scripts/Character/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {  
 
-    private float speed = 0.1f/0.01f;
+    private float speed = 7f;
     public float jumpF = 14f;
     private bool podepular;
     private bool doublejump;
