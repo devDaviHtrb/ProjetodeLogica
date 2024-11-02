17fe51a163921faa919c7d177b3cd19822535d6a
devDaviHtrb <davideleteribeiro1@gmail.com>
2024-11-02 17:42:42 -0300

    nova build

--- Jogo/Assets/Game/Scripts/Game/Controller.cs
+++ Jogo/Assets/Game/Scripts/Game/Controller.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
 public class Controller : MonoBehaviour
 {
     public static Controller Instancia;
@@ -10,6 +12,8 @@ public class Controller : MonoBehaviour
     public GameObject GameOver;
     public GameObject Player;
     public bool Cobol = true;
+    public int temp;
+    public Text tempoTexto;
     void Start()
     {  
         Instancia = this;
@@ -18,16 +22,21 @@ public class Controller : MonoBehaviour
         if(SceneManager.GetActiveScene().name == "Fase1" ||SceneManager.GetActiveScene().name == "Fase2"||SceneManager.GetActiveScene().name == "Menu"||SceneManager.GetActiveScene().name == "MathFase"){
             PlayerPrefs.SetInt("Arma", 0);
         }
+          if(SceneManager.GetActiveScene().name == "Fase1"){
+            PlayerPrefs.SetInt("Tempo", 0);
+          }
+          StartCoroutine(tempo());
     }
     void Update()
     {
         alterInfo(vida, points);
-        if(vida ==0){
+        if(vida <=0){
             GameOver.SetActive(true);
             Destroy(Player);
         }
-
+        
     }
+    
     public void UpdateScoreText(){
         scoreText.text = points.ToString();
     }
@@ -44,4 +53,15 @@ public class Controller : MonoBehaviour
     public void MudarFase(string fase){
         SceneManager.LoadScene(fase);
     }
+
+    private IEnumerator tempo(){
+        yield return new WaitForSeconds(1);
+        if(Cobol == true){
+             temp = PlayerPrefs.GetInt("Tempo");
+             temp++;
+             PlayerPrefs.SetInt("Tempo", temp);
+            tempoTexto.text = temp.ToString();
+        }
+        StartCoroutine(tempo());
+      }
 }
